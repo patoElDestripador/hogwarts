9757542dfa0dd11d4c608564bdc3b4ca2b6ef017
PEPE <patoasesino69@gmail.com>
2023-12-06 17:27:03 -0500

    as

--- index.js
+++ index.js
@@ -12,15 +12,17 @@ class Hogwarts {
     //Front Generadores
     generarTest(){
         this.frontContenedor.innerHTML =  `
-        <div class="row text-light mt-5">
+
+
         <div class="col-12 border border-white text-center">
             Selecciona hasta 5 atributos que te identifiquen
         </div>
         <div class="col-12 border border-white" >
+        <div class="ContendorCheck text-light mt-5">
             <div class="form-check row" id="conetendorCualidades">
             </div>
         </div>
-    </div>`;
+        </div>`;
     console.log("datos",datosCualidades.cualidades)
     for (let index = 0; index < datosCualidades.cualidades.length; index++) {
         document.getElementById("conetendorCualidades").innerHTML += `
