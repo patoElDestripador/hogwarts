d003c24df56733a4078750d7ba75d8682ecb9508
PEPE <patoasesino69@gmail.com>
2023-12-11 17:28:35 -0500

    cambios 2

---
+++ hj.js
@@ -0,0 +1,6 @@
+estudiante = {
+    juego: function bog(){jue[0].juego2(1)},
+    juego2 : function bog2(para){console.log(para)}
+    }
+let jue = [estudiante]
+    
--- index.js
+++ index.js
@@ -11,33 +11,35 @@ class Hogwarts {
             cualidades: [],
             clases :[],
             animalPatronus :"", 
-            
+            genero: ""
         }
         this.informacionCasa = {
             nombre : "",
             estudiantes : 0,
             puntos : 0
         }
-        this.generarVideoIntro()
+        this.contadorTestDePersonalidad = 0;
+        this.cualidadesTestDePersonalidad = [];
+        //this.generarVideoIntro()
         //dependiendo de la casa saldria el personaje
     }
 
 
-    //crud de juego
+
 
     guardarEstudiante(){
         this.estudiante = {
             nombre: document.getElementById("idNombre").value,
             edad:document.getElementById("idFechaNacimiento").value,
-            linaje:this.validarLinaje(),
+            linaje:this.generarLinaje(),
             casa : "",
             cualidades: [],
             clases :[],
             animalPatronus :"", 
-            
+            genero: document.getElementById("idGenero").value
         }
     }
-    validarLinaje(){
+    generarLinaje(){
         let linajepadre = document.getElementById("idFamliaPadre").value;
         let linajemadre = document.getElementById("idFamliaMadre").value;
         //1 = Mestizo  
@@ -57,32 +59,62 @@ class Hogwarts {
         }
     }
     seleccionarCasa(){
-        datosCualidades
-    }
-    GenerarAnimalPatronus(){
 
+        generarTestPersonalidad()
+        
+    }
+
+    validarTestDePersonalidad(valor){
+        let seleciconBoton = document.getElementById(`btn-check-outlined${valor}`).checked
+        let SeleciconCualidad = datosCualidades.cualidades[valor]
+        if(this.cualidadesTestDePersonalidad.length <= 8 && seleciconBoton ){
+            this.cualidadesTestDePersonalidad.push(SeleciconCualidad)
+        }else if(!seleciconBoton){
+            let posicion = this.cualidadesTestDePersonalidad.findIndex(e=> e === SeleciconCualidad)
+            if( posicion != -1)
+            this.cualidadesTestDePersonalidad.splice(posicion,1)
+        }else {
+            console.log("solo puedes elegir 9 cualidades");
+            seleciconBoton = false
+        }
+        console.log(this.cualidadesTestDePersonalidad);
+    }
+
+
+    generarAnimalPatronus(){
+        document.getElementById("contenedor").remevoChild(formualario);
     }
 
     //Front Generadores
-    generarTest(){
-        this.frontContenedor.innerHTML =  `
-
-
-        <div class="col-12 border border-white text-center">
-            Selecciona hasta 5 atributos que te identifiquen
+    generarTestPersonalidad(){
+        let test = document.createElement("div");
+        test.classList = "z-3 position-absolute w-100 mt-5 row"
+        test.innerHTML = `
+        <div class="col-6 offset-3 border border-white text-center text-light mt-5 p-3">
+                <h1>Selecciona hasta 5 atributos que te identifiquen</h1>
         </div>
-        <div class="col-12 border border-white" >
-        <div class="ContendorCheck text-light mt-5">
-            <div class="form-check row" id="conetendorCualidades">
+            <div class="col-6 offset-3 border border-white p-3" >
+                <div class="ContendorCheck text-light mt-5">
+                    <div class="form-check" >
+                        <div class="row" id="conetendorCualidades">
+                        </div>
+                    </div>
+                </div>
+                <div class=" col-6 offset-3 d-grid ">
+                <button class="btn btn btn-outline-light" type="button" onClick="hogwarts.validarTestDePersonalidad()">enviar</button>
+                </div>
             </div>
-        </div>
+
         </div>`;
+        document.getElementById("contenedor").appendChild(test);
     console.log("datos",datosCualidades.cualidades)
     for (let index = 0; index < datosCualidades.cualidades.length; index++) {
         document.getElementById("conetendorCualidades").innerHTML += `
         
-        <input type="checkbox" class="btn-check" id="btn-check-outlined${index}" autocomplete="off">
-        <label class="btn btn-outline-light" for="btn-check-outlined${index}">${datosCualidades.cualidades[index]}</label>
+        <div class="mb-4 col-2 d-grid gap-2">
+            <input type="checkbox" class="btn-check" onChange="hogwarts.validarTestDePersonalidad(${index});" id="btn-check-outlined${index}" autocomplete="off">
+            <label class="btn btn-outline-light" for="btn-check-outlined${index}">${datosCualidades.cualidades[index]}</label>
+        </div>
         `
     }
     }
@@ -107,9 +139,8 @@ class Hogwarts {
 
     generarFormulario(){
         let formualario = document.createElement("div");
-        formualario.classList = "z-3 position-absolute w-100"
+        formualario.classList = "z-3 position-absolute w-100 d-flex justify-content-center align-items-center min-vh-100"
         formualario.innerHTML = `        
-        <div class="d-flex justify-content-center align-items-center min-vh-100">
         <form class="w-50 ms-5 efectoDifuminado p-4 rounded">
             <div class="mb-3 text-center text-light">
                 <h1>Formulario inscripción</h1>
@@ -121,6 +152,15 @@ class Hogwarts {
             <div class="mb-3">
                 <label for="idFechaNacimiento" class="form-label text-light">Fecha nacimiento</label>
                 <input type="date" class="form-control efectoDifuminado text-light" id="idFechaNacimiento">
+            </div>
+            <div class="mb-3">
+            <label for="idGenero" class="form-label text-light">Genero</label>
+                <select class="form-select text-light efectoDifuminado" id="idGenero" aria-label="Default select example">
+                    <option selected>Seleccionar...</option>
+                    <option value="1">Hombre</option>
+                    <option value="2">Mujer</option>
+                    <option value="3">Pato pura</option>
+                </select>
             </div>
             <div class="mb-3">
                 <label for="idFamliaPadre" class="form-label text-light">Linaje familiar padre</label>
@@ -141,14 +181,17 @@ class Hogwarts {
                 </select>
             </div>
             <div class="d-grid gap-2">
-                <button type="button" id="btnSubmit" class="btn btn-secondary" type="button" onClick="hogwarts.guardarEstudiante()">Enviar</button>
+                <button type="button" id="btnSubmit" class="btn btn btn-outline-light" type="button" onClick="hogwarts.guardarEstudiante()">Enviar</button>
             </div>
-        </form>
-        </div>`;
+        </form>`;
         document.getElementById("contenedor").appendChild(formualario);
-        //document.getElementById("contenedor").remevoChild(formualario);
+        
+        this.guardarEstudiante();
+        document.getElementById("contenedor").remevoChild(formualario);
+        this.generarTestPersonalidad()
     }
 
 }
 
 let hogwarts = new Hogwarts();
+hogwarts.generarTestPersonalidad();
